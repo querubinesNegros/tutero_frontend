14ddda7da99b9c7b46b893be5aa20d461696f6f7
David Jose Leon Aroca <djleona@unal.edu.co>
2018-11-12 04:11:45 +0000

    Estadisticas

--- src/componentes/Estadisticas.js
+++ src/componentes/Estadisticas.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
+import MenuAdmin from './MenuAdmin';
 import store from '../store';
 import baseURL from '../url';
 import baseURLFront from '../urlFront';
 import axios from 'axios';
 import {Chart} from 'chart.js'
+import $ from 'jquery'
 
 export default class Estadisticas extends Component {
   
     state = {}
-
+    
     componentDidMount(){
         var _this = this;
         axios.get(`${baseURL}/users/${store.getState().id}/admin/estadisticas.json`)
@@ -21,9 +23,10 @@ export default class Estadisticas extends Component {
         });
     }
     
+    
+    
     test = () =>{
         console.log(this.state);
-        
         var data = this.state.estudiantes_por_PBM;
         var ctx = document.getElementById("myChart").getContext('2d');
         var myChart = new Chart(ctx, {
@@ -60,7 +63,224 @@ export default class Estadisticas extends Component {
                 }
             }
         });
+           $("#Boton1").prop('disabled',true);
+           $("#Boton2").prop('disabled',false);
+           $("#Boton4").prop('disabled',false);
+           $("#Boton3").prop('disabled',false);
+           $("#myChart").show();
+           $("#myChart2").hide();
+           $("#myChart3").hide();
+           $("#myChart4").hide();
+    }
+    
+    test2 = () =>{
+        console.log(this.state);
         
+        var data= this.state.estudiantes_por_estrato;
+        var ctx = document.getElementById("myChart2").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: Object.getOwnPropertyNames(data),
+                datasets: [{
+                    label: '# of Votes',
+                    data: Object.values(data),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 99, 132, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255,99,132,1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+           $("#Boton2").prop('disabled',true);
+           $("#Boton4").prop('disabled',false);
+           $("#Boton1").prop('disabled',false);
+           $("#Boton3").prop('disabled',false);
+           $("#myChart").hide();
+           $("#myChart2").show();
+           $("#myChart3").hide();
+           $("#myChart4").hide();
+    }
+    
+    test3 = () =>{
+        console.log(this.state);
+        
+        var data= this.state.estudiantes_por_edad;
+        var ctx = document.getElementById("myChart3").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: Object.getOwnPropertyNames(data),
+                datasets: [{
+                    label: '# of Votes',
+                    data: Object.values(data),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+           $("#Boton3").prop('disabled',true);
+           $("#Boton2").prop('disabled',false);
+           $("#Boton1").prop('disabled',false);
+           $("#Boton4").prop('disabled',false);
+           $("#myChart2").hide();
+           $("#myChart").hide();
+           $("#myChart3").show();
+           $("#myChart4").hide();
+        
+        
+    }
+    
+    test4 = () =>{
+        console.log(this.state);
+        
+        var data= this.state.frecuencia_temas_tutorias;
+        var ctx = document.getElementById("myChart4").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: Object.getOwnPropertyNames(data),
+                datasets: [{
+                    label: '# of Votes',
+                    data: Object.values(data),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)'
+                        
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+           $("#Boton4").prop('disabled',true);
+           $("#Boton2").prop('disabled',false);
+           $("#Boton1").prop('disabled',false);
+           $("#Boton3").prop('disabled',false);
+           $("#myChart2").hide();
+           $("#myChart").hide();
+           $("#myChart4").show();
+           $("#myChart3").hide();
     }
 
   
@@ -68,9 +288,32 @@ export default class Estadisticas extends Component {
         console.log(this.state);
     return (
       <div>
-        <button class="btn btn-primary" onClick={this.test}>Click</button>
-        <canvas id="myChart" width="400" height="400"></canvas>
+      <MenuAdmin/>
+      <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-3"> 
+                <button id="Boton1" class="btn btn-primary"  onClick={this.test}>PBM </button>
+                </div>
+                <div class="col-md-3">
+                <button id="Boton2" class="btn btn-primary" onClick={this.test2}>Estrato </button>
+                </div>
+                <div class="col-md-3">
+                <button id="Boton3" class="btn btn-primary" onClick={this.test3}>Edad </button>
+                </div>
+                <div class="col-md-3">
+                <button id="Boton4" class="btn btn-primary" onClick={this.test4}>Frecuencia </button>
+                </div>
+        </div>
       </div>
+        
+        
+        <canvas id="myChart" width="400" height="400"></canvas>
+        <canvas id="myChart2" width="400" height="400"></canvas>
+        <canvas id="myChart3" width="400" height="400"></canvas>
+        <canvas id="myChart4" width="400" height="400"></canvas>
+        
+      </div>
+      
     )
   }
 }
