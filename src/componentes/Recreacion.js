ba81a9aaff62a68fae869cf9934d19077b82b25e
serbatero <sisanchezp@unal.edu.co>
2018-12-13 11:56:39 -0500

    Recreacion Corousel

--- src/componentes/Recreacion.js
+++ src/componentes/Recreacion.js
@@ -5,32 +5,44 @@ import '../styles/Recreacion.css';
 import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
 import { logPageView } from '../analytics';
 import { Carousel, CarouselInner, CarouselItem } from "mdbreact";
+import axios from "axios";
+import Corosuer from "./Corosuer";
+import baseURL from "../url";
 
-export default class Recreacion extends Component{
+export default class Recreacion extends Component {
 
-    constructor(){
+    constructor() {
         super();
         logPageView();
+        this.state = { recreations: [] };
+    }
+
+    componentWillMount() {
+        var config = {
+            headers: { Authorization: "bearer " + localStorage.getItem("jwtToken") }
+        };
+
+        axios
+            .get(`${baseURL}/recreations`, config)
+            .then(response => {
+                this.setState({ recreations: response.data.recreations });
+                console.log(response.data.recreations);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     render() {
         return (
             <div>
-                <Menu2/>
-                <Carousel activeItem={1} length={3} showControls={true} showIndicators={true} thumbnails className="z-depth-1">
-                    <CarouselInner>
-                    <CarouselItem itemId="1">
-                        <img className="d-block w-100" src="http://bienestar.bogota.unal.edu.co/ADJUNTOS/c56_2609181224_adj_cvc.jpg" alt="First slide" />
-                    </CarouselItem>
-                    <CarouselItem itemId="2">
-                        <img className="d-block w-100" src="http://bienestar.bogota.unal.edu.co/ADJUNTOS/e8a_2708181236_adj_cvc.png" alt="Second slide" />
-                    </CarouselItem>
-                    <CarouselItem itemId="3">
-                        <img className="d-block w-100" src="/karaoke.png" alt="Third slide" />
-                    </CarouselItem>
-                    </CarouselInner>
-                </Carousel>
-                <Footer/>
+                <Menu2 />
+                <div className="row justify-content-md-center">
+                    <div className="col-md-8 mb-3">
+                        <Corosuer images={this.state.recreations} type={1} />
+                    </div>
+                </div>
+                <Footer />
             </div>
         )
     }
