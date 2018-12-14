116e045928ad7f6f6b9ecb373e7fe922be3a39c5
David Jose Leon Aroca <djleona@unal.edu.co>
2018-12-14 12:09:48 +0000

    Estilo en peticion de servicios

--- src/componentes/Servicios.js
+++ src/componentes/Servicios.js
@@ -12,6 +12,7 @@ import axios from 'axios';
 import { logPageView } from '../analytics';
 import Paginacion from './Paginacion'
 import ItemListPost from './ItemListPost'
+import $ from 'jquery';
 
 
 
@@ -26,6 +27,8 @@ export default class Servicios extends Component {
         page: 1, //pagina en la que está en este instante
         cantidad: 0
     }
+    
+    
     componentDidMount() {
         axios.get(`${baseURL}/posts/pages`)
             .then(res => {
@@ -82,10 +85,104 @@ export default class Servicios extends Component {
                 console.log(error);
             });
     }
+    
+    
+    
     render() {
         const cant = this.state.cantidad;
         const page = this.state.page
+        $(document).ready(function(){
+
+$("#slideshow > div:gt(0)").hide();
+
+var buttons = "<button class=\"slidebtn prev\"><i class=\"fa fa-chevron-circle-left\"></i></button><button class=\"slidebtn next\"><i class=\"fa fa-chevron-circle-right\"></i></button\>";
+
+var slidesl = $('.slideitem').length
+var d = "<li class=\"dot active-dot\">&bull;</li>";
+for (var i = 1; i < slidesl; i++) {
+  d = d+"<li class=\"dot\">&bull;</li>";
+}	
+var dots = "<ul class=\"slider-dots\">" + d + "</ul\>";
+
+$("#slideshow").append(dots).append(buttons);
+var interval = setInterval(slide, 3000);
+
+function intslide(func) {
+	if (func == 'start') { 
+ 	interval = setInterval(slide, 3000);
+	} else {
+		clearInterval(interval);		
+		}
+}
+
+function slide() {
+		sact('next', 0, 1200);
+}
+	
+function sact(a, ix, it) {
+        var currentSlide = $('.current');
+        var nextSlide = currentSlide.next('.slideitem');
+        var prevSlide = currentSlide.prev('.slideitem');
+		    var reqSlide = $('.slideitem').eq(ix);
+
+		    var currentDot = $('.active-dot');
+    	  var nextDot = currentDot.next();
+    	  var prevDot = currentDot.prev();
+		    var reqDot = $('.dot').eq(ix);
+		
+        if (nextSlide.length == 0) {
+      		nextDot = $('.dot').first();
+            nextSlide = $('.slideitem').first();
+            }
+
+        if (prevSlide.length == 0) {
+      		prevDot = $('.dot').last();
+            prevSlide = $('.slideitem').last();
+            }
+			
+		if (a == 'next') {
+			var Slide = nextSlide;
+			var Dot = nextDot;
+			}
+			else if (a == 'prev') {
+				var Slide = prevSlide;
+				var Dot = prevDot;
+				}
+				else {
+					var Slide = reqSlide;
+					var Dot = reqDot;
+					}
+
+        currentSlide.fadeOut(it).removeClass('current');
+        Slide.fadeIn(it).addClass('current');
+		
+    	currentDot.removeClass('active-dot');
+    	Dot.addClass('active-dot');
+}	
+
+$('.next').on('click', function(){
+		intslide('stop');						
+		sact('next', 0, 400);
+		intslide('start');						
+	});//next
+
+$('.prev').on('click', function(){
+		intslide('stop');						
+		sact('prev', 0, 400);
+		intslide('start');						
+	});//prev
+
+$('.dot').on('click', function(){
+		intslide('stop');
+		var index  = $(this).index();
+		sact('dot', index, 400);
+		intslide('start');						
+	});//prev
+//slideshow
+});
+        
         return (
+            
             <div id="containerSer">
                 <Menu2 />
                 <Grid className="grid">
@@ -94,9 +191,9 @@ export default class Servicios extends Component {
                             <Paginacion paginas={cant} actual={page} changePage={this.changePage} />
                         </div>
                     </div>
-                    <Row className="show-grid text-center" >
-                        {this.state.posts.map(home => <Col xs={12} sm={4}>
-                            <ItemListPost home = {home} /> 
+                    <Row id="slideshow" >
+                        {this.state.posts.map(home => <Col xs={1} sm={12}>
+                            <ItemListPost className="slideitem current" home = {home} /> 
                         </Col>
                         )}
                     </Row>
