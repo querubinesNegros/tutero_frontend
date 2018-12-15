223aff9a8681e005ba2f0c267923ff98d5cea7d6
serbatero <sisanchezp@unal.edu.co>
2018-12-15 11:55:36 -0500

    Arreglando answer

--- src/componentes/Answer.js
+++ src/componentes/Answer.js
@@ -5,11 +5,21 @@ export default class Answer extends Component {
   
 
   render() {
+    const respuesta = this.props.user.userable_type
+    let tipo;
+    if(respuesta == "Student"){
+      tipo = "Estudiante"
+    }else if (respuesta == "Tutor"){
+      tipo = "Tutor"
+    }else if (respuesta == "Admin"){
+      tipo = "Administrador"
+    }
+
     return (
       <div className="card">        
         <div className="card-body"> 
           <p className="card-subtitle">{this.props.content}</p>                       
-          <p className="card-text">Usuario:{(this.props.user.userable_type) + " " + 
+          <p className="card-text">Usuario: {(tipo) + " " + 
           (this.props.user.name) + ' ' + (this.props.user.lastname)}</p>                             
         </div>
       </div>       
